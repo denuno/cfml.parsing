b38b4407e052a792407801524f9fe6a318ae8a17
denuno <valliantster@gmail.com>
2010-03-07 15:49:51 -0700

    getting there

---
+++ src/cfml/parsing/CFMLParser.java
@@ -0,0 +1,24 @@
+package cfml.parsing;
+
+public class CFMLParser {
+	
+	public CFMLParser() {
+		
+	}
+	
+	public CFMLParser(String dictionary) {
+	}
+	
+	public void addDictionary(String dictionary) {
+	}
+	
+	public void addCFMLSource(String sourceFile) {
+	}
+	
+	public void parse() {
+	}
+	
+	public void getCFMLTags() {
+	}
+	
+}
